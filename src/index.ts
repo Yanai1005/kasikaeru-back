b15a4183e5ad551c20b30ccd69a7b9fcdb8c26b9
柳井 <108969330+Yanai1005@users.noreply.github.com>
2025-08-16 16:57:06 +0900

    [add] route定義

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import type { HonoEnv } from './types/bindings';
+import users from './routes/users';
+import categories from './routes/categories';
+import objects from './routes/objects';
+import lentRecords from './routes/lentRecords';
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
+const app = new Hono<HonoEnv>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use('/api/*', cors());
+
+app.route('/api/users', users);
+app.route('/api/categories', categories);
+app.route('/api/objects', objects);
+app.route('/api/lent-records', lentRecords);
 
 export default app;
